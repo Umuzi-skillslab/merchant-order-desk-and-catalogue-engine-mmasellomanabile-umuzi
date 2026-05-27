8133f9d7785992b4c467f97deb5bcd1fce28011c
mmasellomanabile-umuzi <mmasello.manabile@umuzi.org>
2026-05-27 14:11:17 +0200

    updated code and followed instructions

--- Product.java
+++ Product.java
@@ -1,5 +1,8 @@
 //Product catalogue
-//Product class
+//1.Product class
+
+import java.util.List;
+
 public class Product {
     private int id;
     private String name;
@@ -28,7 +31,7 @@ public class Product {
     }
 }
 
-//Customer class- using the same process I used above:
+//2.Customer class- using the same process I used above:
 
 class Customer {
     private int id;
@@ -53,3 +56,70 @@ class Customer {
     }
 
 }
+
+//3. Order lifecycle:
+
+class Order {
+    private int id;
+    private Customer customer;
+    private List<OrderItem> items; //copied from instructions 
+
+    //trying to acheive this step by using the constructor from Products
+    public Order(int id, Customer customer){  //Not sure how to add the order item list
+        this.id = id;
+        this.customer = customer;
+        this.items = new ArrayList<>();
+    }
+
+
+    //created addItem as per instructions:
+
+    /*Started here- too many errors: 
+
+    public void addItem(Product product, int quantity){
+        this.product = product;
+        this.quantity = quantity;
+    }
+
+    public Product getProduct () {
+        return product;
+    }
+    
+    */
+
+    public void addItem(Product product, int quantity){
+        items.add(new OrderItem(product,quantity));
+    }
+
+    public String calculateTotal() {
+
+        int total = 0; 
+        
+        for (OrderItem item : items) {
+        total += item.calcTotal();
+    }
+
+     
+}
+
+//4. orderItem class: 
+
+class OrderItem {
+    private Product product;
+    private int quantity;
+
+    
+    public Product getProduct() {
+    return product;
+    }
+
+    public int getQuantity() {
+    return quantity;
+    }
+
+
+    public double calcTotal() {
+        return product.getPrice()*quantity;
+    }
+}
+
